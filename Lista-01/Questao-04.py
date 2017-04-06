d41db1e46dbdc7e062d1510299366a9d4bc0cc2c
Gabriel Sarmento <gabriel77077@gmail.com>
2017-04-06 20:01:45 -0300

    Questao 05 resolvida

---
+++ Lista-01/Questao-04.py
@@ -0,0 +1,11 @@
+#Ler dois números inteiros, x e y, e imprimir o quociente e o resto da divisão inteira entre
+#eles.
+
+x = input("Digite um número inteiro: ")
+y = input("Digite outro número inteiro: ")
+
+x = int(x)
+y = int(y)
+
+print("Quociente: ", x/y)
+print("Resto: ", x%y)
