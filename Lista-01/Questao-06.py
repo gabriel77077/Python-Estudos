852430d3436958cef3efcc59ab21fd7478af111a
Gabriel Sarmento <gabriel77077@gmail.com>
2017-04-07 17:28:47 -0300

    Questao 06 resolvida

---
+++ Lista-01/Questao-06.py
@@ -0,0 +1,14 @@
+#Para ler dois valores reais do teclado, calcular e imprimir na tela:
+#a) A soma destes valores
+#b) O produto deles
+#c) O quociente entre eles
+
+valor01 = input("Digite um valor real: ")
+valor02 = input("Digite outro valor real: ")
+
+valor01 = float(valor01)
+valor02 = float(valor02)
+
+print("Soma: ",valor01 + valor02)
+print("Produto: ",valor01 * valor02)
+print("Quociente: ",valor01 / valor02)
