f36fc5ca73c387ae3ee0dffaed6c398f12cecd94
Gabriel Sarmento <gabriel77077@gmail.com>
2017-04-08 17:29:21 -0300

    Questao 09 resolvida

---
+++ Lista-01/Questao-09.py
@@ -0,0 +1,8 @@
+#Ler um nome do teclado e ver se é igual ao seu nome. Imprimir conforme o caso: “NOME
+#CORRETO” ou “NOME INCORRETO”.
+
+nome = input("Tente adivinhar meu primeiro nome: ")
+if nome == 'gabriel':
+    print("NOME CORRETO")
+else:
+    print("NOME INCORRETO")
