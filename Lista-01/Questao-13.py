86916098ac71e8f1e615bdd608905c5c8625d405
gabrielsarmento <gabriel77077@gmail.com>
2017-04-17 16:18:09 -0300

    Questao 13 resolvida

---
+++ Lista-01/Questao-13.py
@@ -0,0 +1,10 @@
+# Sabendo que latão é constituído de 70% de cobre e 30% de zinco, indique a quantidade de
+# cada um desses componentes para se obter uma certa quantidade de latão (requerida pelo
+# usuário).
+quantidade = float(input("Digite a quantidadede latão em gramas: "))
+
+cobre = quantidade * 0.7
+zinco = quantidade * 0.3
+
+print("Cobre:",cobre)
+print("Zinco:",zinco)
